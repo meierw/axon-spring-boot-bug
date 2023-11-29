01a375a16f2ba7d1720bb6af7a4a9b9c51d46ee2
Walter Meiren <valters.meirens@gmail.com>
2023-11-29 19:38:07 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.6"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.20"
+	kotlin("plugin.spring") version "1.9.20"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation(platform("org.axonframework:axon-bom:4.9.0"))
+	implementation("org.axonframework:axon-spring-boot-starter")
+	implementation("org.axonframework.extensions.kotlin:axon-kotlin")
+	implementation("org.axonframework.extensions.reactor:axon-reactor-spring-boot-starter")
+
+	testImplementation("org.axonframework:axon-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
